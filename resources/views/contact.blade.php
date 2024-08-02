3e8b55d0a6e1ce8887011edbab3898d0149e3c1c
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-08-01 23:17:34 -0600

    Making first layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Contact</h1>
-</body>
-</html>
+@extends('layouts.landing')
+
+@section('title','contact')
+
+@section('content')
+<h1>Contact page</h1>
+@endsection
