81af3b4e0a9397858614afa414123b61273d9f7a
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-08-01 22:57:22 -0600

    adding more views

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','index')->name('index');
+Route::view('/about','about')->name('about');
+Route::view('/services','services')->name('services');
+Route::view('/contact','contact')->name('contact');
