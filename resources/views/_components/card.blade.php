16537befcd316f2a2e8e2ceb019073001facf99e
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-08-02 20:31:15 -0600

    learning about assets in laravel using statics things, for examples images and css styles

--- resources/views/_components/card.blade.php
+++ resources/views/_components/card.blade.php
@@ -1,5 +1,6 @@
 <div style="border: 1px solid black; border-radius: 10px; padding: 10px; margin: 5px">
     <h3>{{ $title }}</h3>
+    <img src="{{ asset('assets/img/paisaje.png') }}" alt="example" width="128">
     <p>
         {{ $content }}
     </p>
