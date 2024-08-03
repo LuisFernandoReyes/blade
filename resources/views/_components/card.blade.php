37b389917749ae5b6293fdccd0caf114f0d64751
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2024-08-02 20:15:43 -0600

    adding components

---
+++ resources/views/_components/card.blade.php
@@ -0,0 +1,6 @@
+<div style="border: 1px solid black; border-radius: 10px; padding: 10px; margin: 5px">
+    <h3>{{ $title }}</h3>
+    <p>
+        {{ $content }}
+    </p>
+</div>
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,5 +3,13 @@
 @section('title','services')
 
 @section('content')
-<h1>Services page</h1>
+    <h1>Services</h1>
+    @component('_components.card')
+        @slot('title','Service 1')
+        @slot('content','Lorem ipsum dolor sit amet, consectetur adipisicing elit.')
+    @endcomponent
+    @component('_components.card')
+        @slot('title','Service 2')
+        @slot('content','Lorem 2 ipsum dolor sit amet, consectetur adipisicing elit.')
+    @endcomponent
 @endsection
